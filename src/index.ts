4cdabb31e6cd5633b80438d367f532adc89dd615
DigVijayWa <digvijaywadkar24@gmail.com>
2021-04-11 01:17:02 +0530

    Added Message types

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,18 @@ import express from "express";
 import expressWs from "express-ws";
 import WebSocket, { Server /* etc */ } from "ws";
 
-import { ConnectedClients, Packet, InitialMessage } from "./types/types";
+import {
+  ConnectedClients,
+  Packet,
+  InitialMessage,
+  PlayerMessage,
+} from "./types/Types";
 
 const { app, getWss, applyTo } = expressWs(express());
 
 const port = 8080;
 
-const connectedClients: ConnectedClients[] = [];
+let connectedClients: ConnectedClients[] = [];
 
 app.ws("/connect", (ws, req) => {
   // push the upcoming websocket connection to the existing clientList.
@@ -18,22 +23,43 @@ app.ws("/connect", (ws, req) => {
     ipAddress: req.connection.remoteAddress,
   });
 
-  ws.on("message", (message) =>{
+  ws.on("message", (message) => {
     // tslint:disable-next-line:no-console
     console.log("received: %s", message);
     sendAll({
+      type: "PACKET",
       id: new Date().getTime(),
       data: message.toString(),
       length: message.toString().length,
     });
   });
 
-  ws.send({message: "Connected", validity: 1000000});
+  ws.on("close", (message) => {
+    connectedClients = removeConnectedClient(message.toString());
+
+    sendAll({
+      type: "PLAYER_LEFT",
+      id: new Date().getTime(),
+      leftPlayer: message.toString(),
+      length: message.toString().length,
+    });
+  });
+
+  sendAll({
+    type: "PLAYER_JOINED",
+    id: new Date().getTime(),
+    joinedPlayer: req.query.id as string,
+    length: req.query.id.toString().length,
+  });
 });
 
-const sendAll = (packet: Packet) => {
+const sendAll = (packet: PlayerMessage) => {
   connectedClients.forEach((item) => item.webSocket.send(packet));
-}
+};
+
+const removeConnectedClient = (connectedClientId: string) => {
+  return connectedClients.filter((item) => !(item.id === connectedClientId));
+};
 
 // adding websocket.
 
--- src/types/Types.ts
+++ src/types/Types.ts
@@ -1,10 +1,26 @@
 import WebSocket, { Server /* etc */ } from "ws";
 
 export type Packet = {
+  type: string;
   data: string;
   id: number;
   length: number;
 };
+export type PlayerJoined = {
+  type: string;
+  joinedPlayer: string,
+  id: number;
+  length: number;
+};
+
+export type PlayerLeft = {
+  type: string;
+  leftPlayer: string,
+  id: number;
+  length: number;
+};
+
+export type PlayerMessage = PlayerJoined | PlayerLeft | Packet;
 
 export type ConnectedClients = {
   webSocket: WebSocket;
@@ -16,3 +32,5 @@ export type InitialMessage = {
   message: string;
   validity: number;
 };
+
+
