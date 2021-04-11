cd5281bf0736fc68def24f8e26f786789f7a3809
DigVijayWa <digvijaywadkar24@gmail.com>
2021-04-11 22:13:37 +0530

    encoder changes

--- src/utility/Utility.ts
+++ src/utility/Utility.ts
@@ -1,20 +1,52 @@
 import { Packet, PlayerData } from "../types/Types";
-import { match, select } from 'ts-pattern';
+import { match, select } from "ts-pattern";
 
 export const playerMessageToString = (playerMessage: Packet) => {
-    return match(playerMessage)
-    .with({type: 'PACKET'}, (res) => `{"correlationId": ${res.correlationId}, "type": ${res.type}, "id": "${res.id}", "data": ${playerDataToString(res.data)}, "length": ${playerDataToString(res.data).length}}`)
-    .with({type: 'PLAYER_JOINED'}, (res) => `{"type": ${res.type}, "correlationId": ${res.correlationId}, "id": "${res.id}", "length": ${res.id.length}}, "data": ${playerDataToString(res.data)}`)
-    .with({type: 'PLAYER_LEFT'}, (res) => `{"type": ${res.type}, "correlationId": ${res.correlationId}, id: "${res.id}", "length": ${res.id.length}}, "data": ${playerDataToString(res.data)}`)
-    .otherwise(() => 'INVALID');
-}
-
+  return match(playerMessage)
+    .with(
+      { type: "PACKET" },
+      (res) =>
+        `{
+            "correlationId": ${res.correlationId}, 
+            "type": ${res.type}, 
+            "id": "${res.id}", 
+            "data": ${playerDataToString(res.data)}, 
+            "length": ${playerDataToString(res.data).length}
+        }`
+    )
+    .with(
+      { type: "PLAYER_JOINED" },
+      (res) =>
+        `{
+            "type": ${res.type}, 
+            "correlationId": ${res.correlationId}, 
+            "id": "${res.id}", 
+            "length": ${res.id.length}, 
+            "data": ${playerDataToString(res.data)}
+        }`
+    )
+    .with(
+      { type: "PLAYER_LEFT" },
+      (res) =>
+        `{
+            "type": ${res.type}, 
+            "correlationId": ${res.correlationId}, 
+            "id": "${res.id}", 
+            "length": ${res.id.length}, 
+            "data": ${playerDataToString(res.data)}
+        }`
+    )
+    .otherwise(() => "INVALID");
+};
 
 export const playerDataToString = (playerData: PlayerData) => {
-    return `{ "xPosition": ${playerData.xPosition}, "yPosition": ${playerData.yPosition}}`
-}
+  return `{ 
+      "xPosition": ${playerData.xPosition}, 
+      "yPosition": ${playerData.yPosition}
+    }`;
+};
 
 export const getDataFromMessage = async (data: string) => {
-   const parsedJson = await JSON.parse(data);
-   return parsedJson;
-}
+  const parsedJson = await JSON.parse(data);
+  return parsedJson;
+};
