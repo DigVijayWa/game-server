a00db3868c96e6bbb9b906c19c0c89d44ce24613
DigVijayWa <digvijaywadkar24@gmail.com>
2021-04-16 00:53:07 +0530

    adding setup file for jest

--- jest.config.integration.js
+++ jest.config.integration.js
@@ -2,6 +2,7 @@ module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
     roots: ["__tests__/integration/"],
+    setupFilesAfterEnv: ['./jest.setup.js'],
   };
 
   jest.setTimeout(10000);
